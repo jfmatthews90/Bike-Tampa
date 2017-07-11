142c13e127442d3cb5f2932a60ecee4c5dfa7537
James <jfmatthews90@gmail.com>
2017-07-10 21:24:57 -0400

    3.8 Create about and contact page. Rootes.rb to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
+  # Added from task 3.8. ***This is causing a 500 internal server error***
+	#root 'static_pages#index'
+	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
