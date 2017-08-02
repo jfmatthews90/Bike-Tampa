bda06a858cda5c40a756f258f6bf20e36d639340
James <jfmatthews90@gmail.com>
2017-08-02 11:24:01 -0400

    5.1 Changed root of route.rb file. Changed index.html.erb nav links to dynamic erb code

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,12 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
+  get 'products/index'
+
+  # get 'products/:id', to: 'products#show' // example standard route per task 5.1
+
 	root 'static_pages#landing_page'
+	# root 'products#index' // example of new route for 5.1
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
