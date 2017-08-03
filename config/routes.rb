bfc2f720daede9021c39fff7cacfdb14b0e0c30e
James <jfmatthews90@gmail.com>
2017-08-03 10:53:32 -0400

    5.1 Changed root to static_pages#index. Changed nav links to dynamic erb in application.html.erb file.

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   get 'static_pages/index'
 
-	root 'static_pages#landing_page'
+	#root 'static_pages#landing_page' 
+	#commented out for 5.1 task
+	root 'static_pages#index'
 	
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
