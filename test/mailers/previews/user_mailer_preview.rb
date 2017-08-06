70fb1abc7541e54505cef4e62d9e16ed7e4b3ad1
James <jfmatthews90@gmail.com>
2017-08-06 17:21:39 -0400

    5.3 Created and updated contact_form.html.erb file to user_mailer view.

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer
 class UserMailerPreview < ActionMailer::Preview
-
+	def contact_form
+  	UserMailer.contact_form("jfmatthews90@gmail.com", "James", "This is a test message.")
+  end
 end
