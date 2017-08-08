ce9a557b1e72c518dd0ce5835e8852c2b3442206
James <jfmatthews90@gmail.com>
2017-08-08 19:05:54 -0400

    5.3 Made adjustment to product.rb search model method.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,17 @@ class Product < ApplicationRecord
 	has_many :orders
 
 	def self.search(search_term)
-  	Product.where("name LIKE ?", "%#{search_term}%")
-	end
+    if Rails.env.development?
+        Product.where("name LIKE ?", "%#{search_term}%")
+    else #Rails.env.production?
+        Product.where("name ilike ?", "%#{search_term}%")
+      #  end
+      #if search
+      #  find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
+      #  find(:all, :conditions => ['name ilike ?', "%#{search}%"])
+      #else
+      #  find(:all)
+    end
+  end
 end
 
