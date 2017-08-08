02e11a82726b9c2176886bc401149c806fc06ceb
James <jfmatthews90@gmail.com>
2017-08-08 15:48:02 -0400

    5.3 Updated model product.rb to include if/else statement for search method.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,11 @@
 class Product < ApplicationRecord
-#	has_many :orders
+has_many :orders
 end
 
 def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
+  if Rails.env.development?
+    Product.where("name LIKE ?", "%#{search_term}%")
+  else #Rails.env.production?
+    Product.where("name ilike ?", "%#{search_term}%")
+  end
 end
