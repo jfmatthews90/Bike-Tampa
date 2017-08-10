345ed83e336babe42e758abca72eb538df6e4a79
James <jfmatthews90@gmail.com>
2017-08-10 15:10:59 -0400

    5.4 Updated users_controller to not include an admin before_Action.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,4 @@ class ApplicationController < ActionController::Base
  	rescue_from CanCan::AccessDenied do |exception|
   	redirect_to main_app.root_url, alert: exception.message
 	end
-
-	def is_admin?
-  	unless user_signed_in? && current_user.admin?
-  		redirect_to root_url, notice: 'Sorry, you do not have permission to do that.'
-  	end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:show, :index]
+  #before_filter :is_correct_user?
   load_and_authorize_resource
 
   # GET /users
@@ -71,6 +72,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email  )
+      params.require(:user).permit(:first_name, :last_name)
     end
 end
