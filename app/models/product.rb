8ddcdcc63939e83d0c18784ea2e2fc4d40d2ad60
James <jfmatthews90@gmail.com>
2017-08-10 09:03:47 -0400

    5.3 Updated static_pages controller with new thank_you method/action.

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,12 +6,6 @@ class Product < ApplicationRecord
         Product.where("name LIKE ?", "%#{search_term}%")
     else #Rails.env.production?
         Product.where("name ilike ?", "%#{search_term}%")
-      #  end
-      #if search
-      #  find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-      #  find(:all, :conditions => ['name ilike ?', "%#{search}%"])
-      #else
-      #  find(:all)
     end
   end
 end
