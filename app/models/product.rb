74c90a89021b177d14e370c0c1bed59b66cf1ba2
James <jfmatthews90@gmail.com>
2017-08-13 20:41:44 -0400

    5.7 Removed highest and lowest rating method variables for test.

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,11 +11,11 @@ class Product < ApplicationRecord
   end
 
   def highest_rating_comment
-  	comments.rating_desc.first
+
 	end
 
 	def lowest_rating_comment
-		comments.rating_asc.first
+
 	end
 end
 
