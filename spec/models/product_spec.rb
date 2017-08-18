cc5f34fb42f3548ab8a9983644eb346f291dbfa8
James <jfmatthews90@gmail.com>
2017-08-17 20:43:13 -0400

    6.2 Added first spec unit test for product_spec.rb.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,22 +3,21 @@ require 'rails_helper' # Gets access to the configuration we need to write RSpec
 describe Product do
 	context "when the product has comments" do
 
+		let(:product) { Product.create!(name: "race bike", description: "a cool race bike.", price: "499.99", image_url: "bike.jpg") }
+		let(:user) { User.create!(email: "test9@test.com", password: "asdasdasd") }
+
 		before do
-			@product = Product.create!(name: "race bike")
-			@user = User.create!(email: "test9@test.com", password: "Soccer801")
 			product.comments.create!(rating: 1, user: user, body: "Awful bike!")
   		product.comments.create!(rating: 3, user: user, body: "Ok bike!")
   		product.comments.create!(rating: 5, user: user, body: "Great bike!")
   	end
 
-  	it "returns the average of all comments" do
-			expect(@product.average_rating). to eq 3
+  	it "returns the average rating of all comments" do
+			expect(product.average_rating).to eq (3)
 		end
 
-		let(:product) { Product.create!(name: "race bike") }
-
-		it "should return race bike" do
-			expect(product.name).to eq "race bike"
+		it "is not valid" do
+			expect(Product.new(description: "moo")).not_to be_valid
 		end
 	end
 end
